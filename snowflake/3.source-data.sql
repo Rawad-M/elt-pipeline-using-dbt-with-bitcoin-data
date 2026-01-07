6ed6a968bd2a0b0bbacb489a4e74f8a5bc6afe4b
Rawad-M <rawad.i.malik@gmail.com>
2026-01-07 18:59:07 +0000

    Creates a task to read the data every 1 hour
    Signed-off-by: Rawad-M <rawad.i.malik@gmail.com>

--- snowflake/3.source-data.sql
+++ snowflake/3.source-data.sql
@@ -1,7 +1,7 @@
 -- Step 1
 -- Create the source table for Bitcoin transactions to be used by DBT
 CREATE
-OR REPLACE TABLE BTC.BTC_SCHEMA.BTC (
+OR REPLACE TABLE BTC_DATABASE.BTC_SCHEMA.BTC (
   HASH_KEY VARCHAR,
   BLOCK_HASH VARCHAR,
   BLOCK_NUMBER INT,
@@ -16,7 +16,7 @@ OR REPLACE TABLE BTC.BTC_SCHEMA.BTC (
 
 -- Step 2
 -- Copy data from stage into the BTC table, with pattern filter for file names for the daily transactions
-COPY INTO BTC.BTC_SCHEMA.BTC
+COPY INTO BTC_DATABASE.BTC_SCHEMA.BTC
     FROM (
     SELECT
     t.$1:hash AS hashkey,
@@ -29,6 +29,31 @@ COPY INTO BTC.BTC_SCHEMA.BTC
     ROUND(t.$1:fee / t.$1: size, 12) AS fee_per_byte,
     t.$1:is_coinbase,
     t.$1:outputs
-    FROM @BTC.BTC_SCHEMA.BTC_STAGE/transactions t
+    FROM @BTC_DATABASE.BTC_SCHEMA.BTC_STAGE/transactions t
+    )
+    PATTERN = '.*/[0-9]{6,7}[.]snappy[.]parquet';
+
+
+-- Step 3
+-- Create a Snowflake Task to copy Bitcoin data every 1 hour
+CREATE
+OR REPLACE TASK BTC_DATABASE.BTC_SCHEMA.BTC_LOAD_TASK
+  WAREHOUSE = LARGE_WH
+  SCHEDULE = '1 HOUR'
+AS
+COPY INTO BTC_DATABASE.BTC_SCHEMA.BTC
+    FROM (
+        SELECT
+        t.$1:hash AS hashkey,
+        t.$1:block_hash,
+        t.$1:block_number,
+        t.$1:block_timestamp,
+        t.$1:fee,
+        t.$1:input_value,
+        t.$1:output_value AS output_btc,
+        ROUND(t.$1:fee / t.$1: size, 12) AS fee_per_byte,
+        t.$1:is_coinbase,
+        t.$1:outputs
+        FROM @BTC_DATABASE.BTC_SCHEMA.BTC_STAGE/transactions t
     )
     PATTERN = '.*/[0-9]{6,7}[.]snappy[.]parquet';
