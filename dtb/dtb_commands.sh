0f109fbaec9f602a27a2fc9a641302290b567e52
Rawad-M <rawad.i.malik@gmail.com>
2026-01-21 21:09:25 +0000

    Updated command
    Signed-off-by: Rawad-M <rawad.i.malik@gmail.com>

--- dtb/dtb_commands.sh
+++ dtb/dtb_commands.sh
@@ -1,10 +1,10 @@
 # Test DBT connection to Snowflake
 dbt debug
 
-# Test DBT creation of test model
+# Test DBT creation of BTC transaction model
 dbt clean
-dbt compile --select 1_btc_transaction_table
-dbt run --select 1_btc_transaction_table
+dbt compile --select transaction_table
+dbt run --select transaction_table
 
 # Check BTC_TRANSACTIONS table freshness via DBT
 dbt source freshness
