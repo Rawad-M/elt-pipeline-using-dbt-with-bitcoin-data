c908d97db74ca5e3171984b29fef212a45a8578f
Rawad-M <rawad.i.malik@gmail.com>
2026-01-24 17:30:31 +0000

    Added incremental model
    Signed-off-by: Rawad-M <rawad.i.malik@gmail.com>

--- bitcoin_dbt_project/models/transaction_table.sql
+++ bitcoin_dbt_project/models/transaction_table.sql
@@ -1,2 +1,20 @@
+-- Controls how DBT writes new BITCOIN transaction data to the table
+{{
+  config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = 'HASH_KEY'
+  )
+}}
+
 SELECT *
 FROM {{ source('raw_btc_source', 'BTC_TRANSACTIONS') }}
+
+-- This is needed so dbt knows which rows are new, otherwise it will append everything again from staging source.
+-- It tells DBT what data is new from staging source.
+-- Outcome is oOnly pull rows from staging that are newer than what’s already in this table.
+    {% if is_incremental() %}
+WHERE BLOCK_TIMESTAMP
+    >
+    (select max (BLOCK_TIMESTAMP) FROM {{ this }})
+    {% endif %}
