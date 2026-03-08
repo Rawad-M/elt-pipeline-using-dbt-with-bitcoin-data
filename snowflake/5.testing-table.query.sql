ead4ce2f8a9954d4477730087b4a0fa98020eec3
Rawad-M <rawad.i.malik@gmail.com>
2026-03-08 10:58:49 +0000

    Added seeds query
    Signed-off-by: Rawad-M <rawad.i.malik@gmail.com>

--- snowflake/5.testing-table.query.sql
+++ snowflake/5.testing-table.query.sql
@@ -14,3 +14,9 @@ SELECT * FROM BTC_DATABASE.DBT_SCHEMA.STG4_TRANSACTION_ALERT_TABLE LIMIT 10;
 -- bc1qx2x5cqhymfcnjtg902ky6u5t5htmt7fvqztdsm028hkrvxcl4t2sjtpd9l
 -- bc1q7cyrfmck2ffu2ud3rn5l5a8yv6f0chkp0zpemf
 -- bc1qm34lsc65zpw79lxes69zkqmk6ee3ewf0j77s3h
+
+
+---------------------------
+-- SEEDS DATA
+---------------------------
+SELECT * FROM BTC_DATABASE.DBT_SCHEMA.BTC_USD_MAX LIMIT 5;
